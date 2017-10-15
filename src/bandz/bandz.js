05307f2bf364e03788f203dbe6ded66bb5d99d01
dalanao <dal437@nyu.edu>
2017-10-15 01:19:25 -0400

    bandz.js site is showing up with values. Now working on GET and POST to function properly.

--- src/bandz/bandz.js
+++ src/bandz/bandz.js
@@ -7,14 +7,14 @@ const publicPath = path.resolve(__dirname, "public");
 const app = express();
 const bodyParser = require('body-parser');
 
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
 
 app.use(express.static(publicPath));
 app.use(function(req, res, next) {
    console.log(req.method, req.path);
    next();
 });
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 /*let bands = [name: "Santana",
 genre: "Latin Rock",
@@ -30,15 +30,6 @@ genre: "Pop",
 location: "Los Angeles, CA",
 description: "Clocks was one of their big hits"];*/
 
-/*create a GET form that has a blank action (so that submissions request the same path); this is the filter form:
-use radio buttons to allow the user to choose which category to filter by
-name the radio buttons filterGenre
-the values of the radio buttons should be rock, pop, hip-hop, electronic, metal, and jazz
-create a submit button
-create a POST form that has a blank action (so that submissions request the same path); this is the add form:
-create 2 text input fields with the attributes: name , location and description, respectively
-a set of radio buttons, each with nameed genre …
-the values of the radio buttons should be rock, pop, hip-hop, electronic, metal, and jazz*/
 
 app.get('/', (req, res) => {
 	res.render('layout.hbs');
